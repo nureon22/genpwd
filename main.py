4118b5ee6f12547e6937868f71028971dcea0ab6
nureon22 <phyowailin2004@proton.me>
2024-10-24 11:11:54 +0630

    Much stronger passwords
    * No duplicate characters
    * Default length to 32
    * Print password in colors
    
    Pass all tests in https://passwordmeter.com

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
-#!/bin/python3
+#!/bin/env python3
 
+import sys
 from math import floor
 from random import random
-from sys import argv
-import re
 
 
-def clamp(minimum: int, prefered: int, maximum: int):
-    return min(max(minimum, prefered), maximum)
-
-
-def genpwd(length: int):
-    chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*<>,.:;?_-+=([{}])"
-    chars_length = len(chars)
-    result = []
-
-    for index in range(length):
-        result.append(chars[floor(random() * chars_length)])
-
-    return "".join(result)
+def random_int(length: int):
+    return floor(random() * length)
 
 
 def main():
-    length = 16
+    all_chars = {
+        "upper": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "lower": "abcdefghijklmnopqrstuvwxyz",
+        "number": "0123456789",
+        "symbol": "~`!@#$%^&*_-+=:;<>,.?/(){}[]"
+    }
+    chars_list = "".join(all_chars.values())
 
-    if len(argv) > 1 and re.search(r"^[0-9]+$", argv[1]):
-        length = clamp(8, int(argv[1]), 512)
+    length = 32
+    result = []
 
-    print(genpwd(length))
+    lastchar_category = None
+
+    while length:
+        char = chars_list[random_int(len(chars_list))]
+
+        for [category, color] in [["upper", 93], ["lower", 92], ["number", 94], ["symbol", 95]]:
+
+            # Find out the category of randomly picked character
+            if char in all_chars[category]:
+
+                # If the last character's category is same as current, regenerate
+                if lastchar_category == category:
+                    length = length + 1
+                    break
+                else:
+                    lastchar_category = category
+                    result.append("\033[{}m{}\033[00m".format(color, char))
+
+                    # Remove randomly picked character from chars_list
+                    # to prevent duplicate characters in final result
+                    chars_list = chars_list.replace(char, "")
+
+        length = length - 1
+
+    print("".join(result))
 
 
 if __name__ == "__main__":
