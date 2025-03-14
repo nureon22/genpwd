1fd67e4adc4699aadecc6159503e9811bf60d60e
Phyo Wai Lin <phyowailin2004@proton.me>
2025-03-14 22:33:09 +0630

    Add support for generating multiple passwords
    Now, you can generate more than one passwords (up to 20) at once using -n, --count option.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from math import floor
 from random import random
 
 
-VERSION="1.1.0"
+VERSION="1.2.0"
 
 
 def random_int(length: int):
@@ -69,13 +69,18 @@ def main():
     arg_parser.add_argument("-c", "--color", action="store_true", help="print password in colors")
     arg_parser.add_argument("-S", "--nosymbols", action="store_true", help="exclude special symbol characters")
     arg_parser.add_argument("-l", "--length", action="store", type=int, default=32, help="password length (from 8 to 60)")
+    arg_parser.add_argument("-n", "--count", action="store", type=int, default=1, help="numbers of passwords to generate (from 1 to 20)")
     arg_parser.add_argument("-v", "--version", action="store_true", help="print version number and exit")
     args = arg_parser.parse_args()
 
     if args.version:
         return print(VERSION)
 
-    genpwd(length=args.length, colored=args.color, nosymbols=args.nosymbols)
+    if args.count:
+        for index in range(min(max(1, args.count), 20)):
+            genpwd(length=args.length, colored=args.color, nosymbols=args.nosymbols)
+    else:
+        genpwd(length=args.length, colored=args.color, nosymbols=args.nosymbols)
 
 
 if __name__ == "__main__":
